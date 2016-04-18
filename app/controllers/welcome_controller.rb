0decd34cf5ad35472966638fd5442d107a81bb3f
Carlos Muiño <camumino@gmail.com>
2016-04-18 21:46:44 +0200

    initial commit with basic demo approximation

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,28 @@
+class WelcomeController < ApplicationController
+  def index
+    @image = RubySvgImageGenerator.create "test", :theme => RubySvgImageGenerator::T_HUMAN_AVATARS
+  end
+
+  def theme
+    @name_theme = params[:theme]
+    theme = "RubySvgImageGenerator::#{@name_theme}".constantize.new
+    @parts = theme.parts
+  end
+
+  def generate
+    parts = []
+
+    params[:parts].each do |part|
+      class_part = part[0].constantize.new
+      parte = class_part.get_matrix(part[1].to_i)
+
+      parts << parte
+    end
+  
+    theme = "RubySvgImageGenerator::#{params['class_theme']}".constantize.new
+    matrix_image = theme.merge_parts(parts)
+    
+    @image = RubyMatrixToSvg.matrix_to_svg('test', matrix_image)
+  end
+
+end
