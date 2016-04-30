a6f5a3ab4c384cf72222ce27ae7dc90ed519c3c8
Carlos Muiño <camumino@gmail.com>
2016-04-30 18:20:26 +0200

    default

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-    @image = '' #RubySvgImageGenerator.create "test", :theme => RubySvgImageGenerator::T_HUMAN_AVATARS
-  end
-
-  def theme
-    @name_theme = params[:theme]
-    @theme = "RubySvgImageGenerator::#{@name_theme}".constantize.new
+    @image = RubySvgImageGenerator.create_random("random_image", :theme => RubySvgImageGenerator::T_HUMAN_AVATARS)
   end
 
   def generate
-    theme = params[:class_theme]
-    parts = params[:parts].collect {|part| part[1].to_i}
-    @image = RubySvgImageGenerator.create("test_image", :theme => theme, :parts => parts)
+    theme_name = params[:theme]
+    @theme = RubySvgImageGenerator.const_get(theme_name).new
+
+    if params[:parts].blank?
+      @image = RubySvgImageGenerator.create_random("random_image", :theme => theme_name)
+    else
+      parts = params[:parts].collect {|part| part[1].to_i}
+      @image = RubySvgImageGenerator.create("test_image", :theme => theme_name, :parts => parts)
+    end
   end
 
 end
